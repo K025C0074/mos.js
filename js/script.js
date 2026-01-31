de76cf6b31ccb5ba5a03f08db05ed9992e8f0730
K025C0074 <k025c0074@g.neec.ac.jp>
2026-02-01 00:03:42 +0900

    hennkou

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var pieData = {
-    labels: ['Webデザイナー', 'Webデベロッパー', 'サーバーエンジニア', '営業職'],
-    series: [14, 9, 8, 6]
-};
-
-var pieOpetions = {
-    width: '100%',
-    height: '440px'
-};
-
-new Chartist.Pie('.pie-chart', pieData, pie)
+new Chartist.Pie('.pie-chart', {
+  // 画像と同じラベル名を設定
+  labels: ['Webデザイナー', 'Webデベロッパー', 'サーバーエンジニア', '営業職'],
+  // グラフの割合（画像に基づいたおおよその比率）
+  series: [35, 25, 20, 20]
+}, {
+  // ラベルをグラフの内側に表示するための設定
+  labelInterpolationFnc: function(value) {
+    return value;
+  }
+});
